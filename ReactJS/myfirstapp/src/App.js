7cc7e5b75802818a846e5a424db98953640abeed
Riya Rathi <158172048+Riya28Rathi@users.noreply.github.com>
2024-09-13 20:43:18 +0530

    Starting
    Starting my React JS Learning Journey.

---
+++ ReactJS/myfirstapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    //JSX -> JS + HTML
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello This is my First React APP...</h1>
+        <h2>WS Cube Tech</h2>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
